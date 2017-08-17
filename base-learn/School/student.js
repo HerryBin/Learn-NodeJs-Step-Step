a8e55b47be72ed39a041861151f0efe4c06802e2
herryBin <xianrongbin1989@126.com>
2017-08-17 22:17:24 +0800

    add

---
+++ base-learn/School/student.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xianrongbin on 2017/7/18.
+ */
+function add(student){
+    console.log('add student '+student);
+}
+
+exports.add=add;
---
+++ base-learn/School/teacher.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xianrongbin on 2017/7/18.
+ */
+function addTeacher(teacher){
+    console.log('add teacher '+teacher);
+}
+
+exports.add=addTeacher;
